303d6113a92741e13816e794d2e2db5ffb9e7003
Evgeny Natarov <skateor2007@gmail.com>
2021-07-26 18:55:25 +0300

    [sk8beemo] add Navigate Button component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <section>
         <add-ticker
-          :token-list="tokenList"
+          :tokenList="tokenList"
           :isUsage="isUsage"
           @add-ticker="add"
           @find-tokens="tokenListHandler"
@@ -14,20 +14,16 @@
       <template v-if="tickers.length">
         <hr />
         <div>
-          <button
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            @click="page = page - 1"
+          <navigate-button
+            :buttonText="'Назад'"
+            @handlePage="page = page - 1"
             v-if="page > 1"
-          >
-            Назад
-          </button>
-          <button
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            @click="page = page + 1"
+          />
+          <navigate-button
+            :buttonText="'Вперед'"
+            @handlePage="page = page + 1"
             v-if="hasNextPage"
-          >
-            Вперед
-          </button>
+          />
           <div>Фильтр: <input v-model="filter" /></div>
         </div>
         <hr class="w-full border-t border-gray-600 my-4" />
@@ -138,6 +134,7 @@
 import { subscribeToTicker, unsubscribeFromTicker } from "./api";
 import AddTicker from "./components/AddTicker.vue";
 import AppSpinner from "./components/AppSpinner.vue";
+import NavigateButton from "./components/NavigateButton.vue";
 
 let coins;
 export default {
@@ -145,7 +142,8 @@ export default {
 
   components: {
     AddTicker,
-    AppSpinner
+    AppSpinner,
+    NavigateButton
   },
 
   data() {
---
+++ src/components/NavigateButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    @click="$emit('handlePage')"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: String
+  },
+  emits: ["handlePage"]
+};
+</script>
+
+<style scoped></style>
