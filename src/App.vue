a54b1668caf383e8cb3cc73a020c913ad9bae193
Evgeny Natarov <skateor2007@gmail.com>
2021-07-26 19:44:35 +0300

    [sk8beemo] add Graph component

--- src/App.vue
+++ src/App.vue
@@ -39,49 +39,13 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div
-          class="flex items-end border-gray-600 border-b border-l h-64"
-          ref="graph"
-        >
-          <div
-            v-for="(bar, idx) in normalizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background:new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <app-graph
+        v-if="selectedTicker"
+        :selectedTicker="selectedTicker"
+        :graph="graph"
+        @shift="graph.shift()"
+        @close="selectedTicker = null"
+      />
     </div>
   </div>
 </template>
@@ -91,11 +55,11 @@
 // [ ] 4. Запросы напрямую внутри компонента (???) | Критичность: 5
 // [ ] 2. При удалении остается подписка на загрузку тикера | Критичность: 5
 // [ ] 5. Обработка ошибок API | Критичность: 5
-// [ ] 3. Количество запросов | Критичность: 4
+// [x] 3. Количество запросов | Критичность: 4
 // [x] 8. При удалении тикера не изменяется localStorage | Критичность: 4
 // [x] 1. Одинаковый код в watch | Критичность: 3
 // [ ] 9. localStorage и анонимные вкладки | Критичность: 3
-// [ ] 7. График ужасно выглядит если будет много цен | Критичность: 2
+// [x] 7. График ужасно выглядит если будет много цен | Критичность: 2
 // [ ] 10. Магические строки и числа (URL, 5000 миллисекунд задержки, ключ локал стораджа, количество на странице) |  Критичность: 1
 
 // Параллельно
@@ -107,6 +71,7 @@ import AddTicker from "./components/AddTicker.vue";
 import AppSpinner from "./components/AppSpinner.vue";
 import NavigateButton from "./components/NavigateButton.vue";
 import AppTicker from "./components/AppTicker.vue";
+import AppGraph from "./components/AppGraph.vue";
 
 let coins;
 export default {
@@ -116,7 +81,8 @@ export default {
     AddTicker,
     AppSpinner,
     NavigateButton,
-    AppTicker
+    AppTicker,
+    AppGraph
   },
 
   data() {
@@ -129,7 +95,6 @@ export default {
       selectedTicker: null,
       tokenList: [],
       graph: [],
-      maxGraphElements: 1,
       page: 1
     };
   },
@@ -166,7 +131,6 @@ export default {
   },
 
   mounted: async function() {
-    window.addEventListener("resize", this.calculateMaxGraphElements);
     this.loading = true;
     await fetch(
       "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
@@ -180,10 +144,6 @@ export default {
           }))
       );
     this.loading = false;
-  },
-
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphElements);
   },
 
   computed: {
@@ -207,19 +167,6 @@ export default {
       return this.filteredTickers.length > this.endIndex;
     },
 
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
-
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -229,22 +176,12 @@ export default {
   },
 
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return;
-      }
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
-    },
-
     updateTicker(tickerName, price) {
       this.tickers
         .filter(t => t.name === tickerName)
         .forEach(t => {
           if (t === this.selectedTicker) {
             this.graph.push(price);
-            if (this.graph.length > this.maxGraphElements) {
-              this.graph.shift();
-            }
           }
           t.price = price;
         });
@@ -297,7 +234,6 @@ export default {
 
     select(ticker) {
       this.selectedTicker = ticker;
-      this.$nextTick().then(this.calculateMaxGraphElements());
     },
 
     handleDelete(tickerToRemove) {
--- src/components/AddTicker.vue
+++ src/components/AddTicker.vue
@@ -35,7 +35,6 @@
         <add-button @click="add()" />
       </div>
     </div>
-    <plus-sign-icon />
   </section>
 </template>
 <script>
